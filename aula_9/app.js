aab631919eb8c1b436bed96588549616e0c58f2c
I'm Frieza Oh Oh Oh <31746868+FranciscoSimaoPerez@users.noreply.github.com>
2018-05-24 20:01:54 +0100

    chat

--- aula_9/app.js
+++ aula_9/app.js
@@ -1,15 +1,47 @@
 var fs = require("fs");
 var data = "O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.";
-var writeStream = fs.createWriteStream('output.txt');
+var writerStream = fs.createWriteStream('x.txt');
+var readerStream = fs.createReadStream('output.txt');
+readerStream.setEncoding('UTF8');
 
-function repeat(){
-    for (var i=0; i<10000; i++){
-        writeStream.write(data,'UTF8');
-    }
-}
-writeStream.end();
-writeStream.on('finish', function(){
+
+// for (var i = 0; i <= 10000; i++) {
+//     writerStream.write(data, 'UTF8');
+// }
+
+// for (var x = 0; x <= 10; x++) {
+//     readerStream.on('data', function (chunk) {
+//         data += chunk;
+//     });
+// }
+
+
+
+// function readFile(fileName){
+//      var file = fs.readFileSync(fileName, 'utf-8');
+//      return file;
+// }
+
+// readerStream.on("data",function(chunk){
+//     writerStream.write(chunk);    
+// });
+
+readerStream.pipe(writerStream);
+
+writerStream.on('finish', function () {
     console.log("write completed.");
 });
 
-console.log(repeat());
+
+readerStream.on('end', function () {
+     writerStream.end();
+    // console.log(data);
+});
+
+writerStream.on('error', function (err) {
+    console.log(err.stack);
+});
+readerStream.on('error', function () {
+    console.log(err.stack);
+});
+
